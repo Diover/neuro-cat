9160e009729c915081f61a3b1a45b2a6b4df8518
Diover <istatishere@gmail.com>
2017-10-23 23:30:37 +0300

    Added a tiny piece of test data for the tensorboard.

--- cats/first.py
+++ cats/first.py
@@ -1,5 +1,17 @@
 import tensorflow as tf
 
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
+def test_tensorboard():
+  a = tf.constant(5)
+  b = tf.constant(4)
+  c = a+b
+  with tf.Session() as sess:
+    Writer = tf.summary.FileWriter("../../log", sess.graph)
+    Writer.close()
+
+def hello_world():
+  hello = tf.constant('Hello, TensorFlow!')
+  sess = tf.Session()
+  print(sess.run(hello))
+
+hello_world()
+test_tensorboard()
